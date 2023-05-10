b55dbea02777f35a90a517a2f5226b6f43b157a2
Jorge E Gomez <jeg1986@gmail.com>
2023-05-10 18:57:33 -0300

    Create ejercicio-002.py
    Enunciado 002 y una solución.

---
+++ ejercicios/ejercicio-002.py
@@ -0,0 +1,12 @@
+"""
+Escribir un programa que solicite al usuario su nombre y su edad, y luego muestre por pantalla un mensaje que diga "Hola, [nombre]. Tu edad es [edad] años."
+Ejemplo de ejecución:
+
+Ingrese su nombre: Juan
+Ingrese su edad: 30
+Hola, Juan. Tu edad es 30 años.
+"""
+
+nombre = input("Inrese su nombre: ")
+edad = input("Ingrese su edad: ")
+print(f"Hola, {nombre}. Tu edad es {edad} años")
