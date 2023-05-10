6737b853ae397bf93735318ab90c81b3430658e3
Jorge E Gomez <jeg1986@gmail.com>
2023-05-10 18:46:32 -0300

    Create ejercicio-001.py
    Enunciado ejercicio 001

---
+++ ejercicios/ejercicio-001.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 001 
+
+Escribir un programa que permita que el usuario ingrese su nombre. El programa debe emitir una salida con un mensaje de bienvenida que incluya el nombre ingresado.
+Ejemplo de ejecución:
+
+    Ingrese su nombre: Juan
+    Bienvenido Juan
+"""
+
+nombre = input("Ingrese su nombre: ")
+print(f"Bienvenido {nombre}")
