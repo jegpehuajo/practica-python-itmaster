8e9ee7e439bbb58adf8b80cb6ae3e49bf224e12a
Jorge E Gomez <jeg1986@gmail.com>
2023-05-10 19:11:14 -0300

    Create ejercicio-004.py

---
+++ ejercicios/ejercicio-004.py
@@ -0,0 +1,8 @@
+"""
+Escribir un programa que solicite al usuario ingresar tres numeros enteros.
+El programa debe mostrar por pantalla el resultado de sumar los tres numeros de la siguiente manera: 
+"[numero1] + [numero2] + [numero3] = [suma]".
+
+Ejemplo: Si el usuario ingresa 1, 2 y 3, el programa debe mostrar por pantalla: "1 + 2 + 3 = 6".
+
+"""
