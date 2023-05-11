3f8a4b7dfd5a4fa4676a5227c84661f92247d4ab
Jorge E Gomez <jeg1986@gmail.com>
2023-05-11 09:58:41 -0300

    Create ejercicio-008.py

---
+++ ejercicios/ejercicio-008.py
@@ -0,0 +1,6 @@
+"""
+Escribir un programa que permita ingresar el valor monetario de una hora de trabajo y 
+la cantidad de horas trabajadas por día, para calcular el salario semanal de un trabajador 
+que trabaja todos los días hábiles y la mitad de las horas del día hábil los sábados, 
+suponiendo que todas las horas tienen el mismo valor."
+"""
