f91aac0554a212589deb2da37194208d0d2cb1fb
Jorge E Gomez <jeg1986@gmail.com>
2023-05-11 09:27:18 -0300

    Create ejercicio-006.py

---
+++ ejercicios/ejercicio-006.py
@@ -0,0 +1,10 @@
+"""
+Escribir un programa que solicite al usuario ingresar tres notas de un alumno. El programa debe mostrar por pantalla las notas separadas por comas en un renglón y el promedio de las notas en el siguiente renglon.
+Ejemplo de ejecución:
+
+    Ingrese la nota 1: 7
+    Ingrese la nota 2: 8
+    Ingrese la nota 3: 9
+    Notas: 7, 8, 9
+    Promedio: 8.0
+"""
