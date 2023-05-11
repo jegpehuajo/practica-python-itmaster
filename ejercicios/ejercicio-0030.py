693105643cc1b93c8fcd4a25cea3027cd26bd047
Jorge E Gomez <jeg1986@gmail.com>
2023-05-11 18:24:40 -0300

    Create ejercicio-0030.py

---
+++ ejercicios/ejercicio-0030.py
@@ -0,0 +1,6 @@
+"""
+Escribir un programa que permita al usuario ingresar dos números enteros. 
+La computadora debe indicar si el mayor es divisible por el menor.
+(Un número entero a es divisible por un número entero b 
+cuando el resto de la división entre a y b es 0)
+"""
