e3dfa15857e49acea431dd96e1e663f3c403c54b
Jorge E Gomez <jeg1986@gmail.com>
2023-05-11 18:17:29 -0300

    Update ejercicio-008.py

--- ejercicios/ejercicio-008.py
+++ ejercicios/ejercicio-008.py
@@ -4,3 +4,9 @@ la cantidad de horas trabajadas por día, para calcular el salario semanal de un
 que trabaja todos los días hábiles y la mitad de las horas del día hábil los sábados, 
 suponiendo que todas las horas tienen el mismo valor."
 """
+
+valor_hora = int(input("Ingrese el valor de la hora: "))
+cantidad_horas_diarias = int(input("Ingrese la cantidad diaria de horas trabajadas: "))
+salario_semanal = valor_hora * cantidad_horas_diarias * 5
+salario_total = salario_semanal + ((cantidad_horas_diarias / 2) * valor_hora)
+print(f"El salario semanal es: {salario_total}")
