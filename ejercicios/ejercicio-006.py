afa57022ece7096ca42e4d07ed0245c3fb099d0c
Jorge E Gomez <jeg1986@gmail.com>
2023-05-11 09:29:31 -0300

    Update ejercicio-006.py

--- ejercicios/ejercicio-006.py
+++ ejercicios/ejercicio-006.py
@@ -1,5 +1,7 @@
 """
-Escribir un programa que solicite al usuario ingresar tres notas de un alumno. El programa debe mostrar por pantalla las notas separadas por comas en un renglón y el promedio de las notas en el siguiente renglon.
+Escribir un programa que solicite al usuario ingresar tres notas de un alumno. 
+El programa debe mostrar por pantalla las notas separadas por comas en un renglón y el promedio de las notas en el siguiente renglon.
+
 Ejemplo de ejecución:
 
     Ingrese la nota 1: 7
@@ -8,3 +10,10 @@ Ejemplo de ejecución:
     Notas: 7, 8, 9
     Promedio: 8.0
 """
+n1 = int(input("Ingrese la nota 1: "))
+n2 = int(input("Ingrese la nota 2: "))
+n3 = int(input("Ingrese la nota 3: "))
+
+promedio = (n1+n2+n3)/3
+
+print(f"Notas: {n1}, {n2}, {n3} ==> promedio: {promedio}.")
