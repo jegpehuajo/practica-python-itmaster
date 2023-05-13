1a222d0e0ca1e23f57192328801e4a63b9e79a08
Jorge E Gomez <jeg1986@gmail.com>
2023-05-13 15:38:49 -0300

    Update ejercicio-0030.py

--- ejercicios/ejercicio-0030.py
+++ ejercicios/ejercicio-0030.py
@@ -4,3 +4,15 @@ La computadora debe indicar si el mayor es divisible por el menor.
 (Un número entero a es divisible por un número entero b 
 cuando el resto de la división entre a y b es 0)
 """
+
+n1 = int(input("Escribir un nro: "))
+n2 = int(input("Escribir un nro: "))
+
+if (n1 >= n2) and (n1 % n2) == 0:
+  print(f"El numero {n1} es mayor a {n2} y son divisibles.")
+elif n2 % n1 == 0:
+  print(f"El numero {n2} es mayor a {n1} y es divisible por {n1}")
+else:
+   print(f"No es divisible el numero {n1} por el {n2}")
+    
+
