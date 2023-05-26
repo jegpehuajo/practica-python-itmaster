fd7da329369807145cf66c7cca5fea03add063cb
Jorge E Gomez <jeg1986@gmail.com>
2023-05-26 17:01:43 -0300

    Create ejercicio-031.py

---
+++ ejercicios/ejercicio-031.py
@@ -0,0 +1,12 @@
+"""
+Una editorial determina el precio de un libro según la cantidad de páginas que contiene. 
+El costo básico del libro es de 1000,más35,10 por página con encuadernación rústica. 
+Si el número de páginas supera las 300 la encuadernación debe ser especial, lo que incrementa el costo en 1200.
+Además,sielnúmerodepáginassobrepasalas600sehacenecesariootroprocedimientodeencuadernaciónqueincrementaelcostoen880. 
+Desarrollar un programa que calcule el costo de un libro dado el número de páginas.
+En Python no existen constantes como tal, pero se utiliza una convención de nomenclatura en 
+mayúsculas para indicar que una variable no debe ser modificada. 
+Esto se conoce como "constante simbólica" o "constante convencional". 
+Para definir una constante convencional, 
+simplemente se define una variable con un nombre en mayúsculas.
+"""
