bdd135509430e966b8937b703d01de7414056fdb
Jorge E Gomez <96948055+jegpehuajo@users.noreply.github.com>
2023-06-28 18:05:14 -0300

    Update ejercicio-038.py

--- ejercicios/ejercicio-038.py
+++ ejercicios/ejercicio-038.py
@@ -3,3 +3,14 @@ Escribir un programa que permita ingresar un número entero n.
 Debe mostrar los primeros 10 múltiplos de n.
 """
 
+CANTIDAD = 10
+
+valor = int(input(f"Ingrese un numero entero para mostrar los primeros {CANTIDAD} multiplos: "))
+
+print("********************")
+
+for v in range(1, CANTIDAD + 1):
+    multiplo = valor * v
+    print(f"{valor} X {v} = {multiplo}")
+
+print("********************")
