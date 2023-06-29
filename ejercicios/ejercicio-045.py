b34e1f71f54f071ad4d13d3a81735031c2930fc5
Jorge E Gomez <96948055+jegpehuajo@users.noreply.github.com>
2023-06-29 09:38:25 -0300

    Create ejercicio-045.py

---
+++ ejercicios/ejercicio-045.py
@@ -0,0 +1,12 @@
+"""
+Escribir un programa que permita leer dos números naturales A y B. 
+Calcular A elevado a la B mediante productos sucesivos y mostrar el resultado.
+Ejemplo:
+
+    4^3 = 4 * 4 * 4 (4 multiplicado 3 veces).
+"""
+
+a = int(input("Ingrese un numero a elevar: "))
+b = int(input("Ingrese el exponente: "))
+
+print(a**b)
