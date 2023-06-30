b50f033a0d24e8384aa1e3c2ee0212fda74dd676
Jorge E Gomez <96948055+jegpehuajo@users.noreply.github.com>
2023-06-30 17:18:58 -0300

    Create ejercicio-054.py

---
+++ ejercicios/ejercicio-054.py
@@ -0,0 +1,8 @@
+"""
+Escribir un programa que permita controlar con validación el ingreso a un sitio web. 
+Teniendo dos constantes con un nombre de usuario ("admin") y una contraseña ("123456"), 
+el programa debe permitir al usuario ingresar sus credenciales. 
+Si las mismas son erróneas, se volverán a pedir hasta un máximo de 3 intentos. 
+Finalmente, la computadora debe mostrar alguno de los siguientes mensajes según sea el caso: 
+"Acceso concedido" o "Se ha bloqueado la cuenta"
+"""
